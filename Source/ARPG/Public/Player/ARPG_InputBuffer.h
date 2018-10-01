3ab9db40ab25a72ac249906d3b97dc783be9ec73
Eragon <450614754@qq.com>
2018-10-01 09:38:34 +0800

    准备制作输入

---
+++ Source/ARPG/Public/Player/ARPG_InputBuffer.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ARPG_InputBuffer.generated.h"
+
+/**
+ * 
+ */
+
+namespace ARPG_InputBuffer
+{
+	UENUM()
+	enum EARPG_InputBuffer
+	{
+		LeftLightAttack,
+		LeftHeavyAttack,
+		RightLightAttack,
+		RightHeavyAttack,
+
+		Forward,
+		Left,
+		Right,
+		Backworld,
+
+		Dodge,
+		Down,
+		Up
+	};
+}
+
+USTRUCT(BlueprintType)
+struct ARPG_API FARPG_InputBuffer
+{
+	GENERATED_BODY()
+
+
+
+
+};
---
+++ Source/ARPG/Public/Player/ARPG_PlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ARPG_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API AARPG_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
