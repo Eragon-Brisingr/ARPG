131873d5f1b109ced12a28cf4676ebaed677195f
Eragon <450614754@qq.com>
2018-10-05 18:54:01 +0800

    添加锁定系统
    Former-commit-id: 0435446c9c224179a5d60b91f8500e75a7bacecc

---
+++ Source/ARPG/Private/Player/ARPG_PlayerControllerBase.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ARPG_PlayerControllerBase.h"
+
+
+
+
+void AARPG_PlayerControllerBase::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (IsLocalController())
+	{
+		LockOnTargetSystem.LockOnTargetTick(this, DeltaSeconds);
+	}
+}
+
+bool AARPG_PlayerControllerBase::SetLockedTarget(AActor* Target, const FName& SocketName)
+{
+	if (LockOnTargetSystem.CanLockedOn(this, Target, SocketName))
+	{
+		LockOnTargetSystem.SetLockedTarget(Target, SocketName);
+		return true;
+	}
+	return false;
+}
---
+++ Source/ARPG/Public/Player/ARPG_LockOnTargetSystem.h
@@ -0,0 +1,78 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ARPG_LockOnTargetSystem.generated.h"
+
+USTRUCT(BlueprintType, meta = (BlueprintInternalUseOnly = "true"))
+struct ARPG_API FARPG_LockOnTargetSystem
+{
+	GENERATED_BODY()
+public:
+	FARPG_LockOnTargetSystem();
+
+	UPROPERTY(BlueprintReadOnly, Category = "锁定系统")
+	TSoftObjectPtr<AActor> LockedTarget;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "锁定系统")
+	FName LockedSocketName;
+
+	UPROPERTY(BlueprintReadWrite, Category = "锁定系统")
+	uint8 bEnableLockOnSystem : 1;
+
+	void LockOnTargetTick(class AController* Controller, float DeltaTime);
+
+	bool CanLockedOn(class AController* Controller, AActor* Target, const FName& SocketName);
+
+	void SetLockedTarget(AActor* Target, const FName& SocketName);
+
+	void ClearLockedTarget();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "锁定系统")
+	float MaxTraceDistance = 1500.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "锁定系统")
+	TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjectTypes;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "锁定系统")
+	TEnumAsByte<ETraceTypeQuery> VisibilityChannel;
+
+	TArray<AActor*> GetIgnoreActors(class AController* Controller) const;
+
+	bool TraceCanLockedActor(class AController* Controller, const FVector& Start, const FVector& End, float Radius = 500.f);
+
+	bool FilterResultToLock(TArray<FHitResult> HitResults, class AController* Controller);
+
+	void ToggleLockedTarget(class AController* Controller);
+};
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UARPG_LockOnTargetInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ARPG_API IARPG_LockOnTargetInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, Category = "锁定系统")
+	FVector GetTargetLocation(const FName& CurLockSocketName) const;
+	virtual FVector GetTargetLocation_Implementation(const FName& CurLockSocketName) const { return FVector::ZeroVector; }
+
+	UFUNCTION(BlueprintNativeEvent, Category = "锁定系统")
+	bool CanLockedOnTarget(AController* Invoker, const FName& InvokeLockedSocketName) const;
+	virtual bool CanLockedOnTarget_Implementation(AController* Invoker, const FName& InvokeLockedSocketName) const { return true; }
+
+	UFUNCTION(BlueprintNativeEvent, Category = "锁定系统")
+	bool CanLockingOnTarget(AController* Invoker, const FName& CurLockSocketName, bool& TryLockAgain) const;
+	virtual bool CanLockingOnTarget_Implementation(AController* Invoker, const FName& CurLockSocketName, bool& TryLockAgain) const { return true; }
+};
