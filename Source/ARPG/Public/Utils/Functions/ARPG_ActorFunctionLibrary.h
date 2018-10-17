a91e7876a211fe66befbcac88a32d82015002a9f
Eragon <450614754@qq.com>
2018-10-17 19:25:00 +0800

    添加ARPG系列函数库

---
+++ Source/ARPG/Public/Utils/Functions/ARPG_ActorFunctionLibrary.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "XD_ActorFunctionLibrary.h"
+#include "ARPG_ActorFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UARPG_ActorFunctionLibrary : public UXD_ActorFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/ARPG/Public/Utils/Functions/ARPG_LevelFunctionLibrary.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "XD_LevelFunctionLibrary.h"
+#include "ARPG_LevelFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UARPG_LevelFunctionLibrary : public UXD_LevelFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
