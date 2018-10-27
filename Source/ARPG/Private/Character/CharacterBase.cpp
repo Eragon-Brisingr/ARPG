b30ba519e77d8d638ff7d41b1c5e42a8a149b680
Eragon <450614754@qq.com>
2018-10-27 14:57:57 +0800

    锁定时角色朝向使用Controller的朝向,非锁定时使用移动方向的朝向

--- Source/ARPG/Private/Character/CharacterBase.cpp
+++ Source/ARPG/Private/Character/CharacterBase.cpp
@@ -51,6 +51,10 @@ void ACharacterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (HasAuthority() || IsLocallyControlled())
+	{
+		ARPG_MovementComponent->SetRotationMode(bIsLockedOther && ARPG_MovementComponent->bInvokeSprint == false ? ECharacterRotationMode::LookingDirection : ECharacterRotationMode::VelocityDirection);
+	}
 }
 
 // Called to bind functionality to input
