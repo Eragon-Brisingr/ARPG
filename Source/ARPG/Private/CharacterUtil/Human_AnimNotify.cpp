020ef092982e7763bfdd55c4b53ef3d7474aeba7
Eragon <450614754@qq.com>
2018-10-28 20:30:53 +0800

    整理Notify系列函数，添加处决伤害通知
    Former-commit-id: a29a7a4c3ce628e7c777c46b300ae51cc511b36b

---
+++ Source/ARPG/Private/CharacterUtil/Human_AnimNotify.cpp
@@ -0,0 +1,183 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Human_AnimNotify.h"
+#include <Components/SkeletalMeshComponent.h>
+#include "HumanBase.h"
+#include "ARPG_WeaponBase.h"
+#include "ARPG_BowBase.h"
+#include "ARPG_ArrowBase.h"
+#include "Animation/AnimInstance.h"
+
+
+void UARPG_DodgeAttack::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (Human->IsLocallyControlled())
+		{
+			if (UAnimMontage* CurPlayingMontage = Cast<UAnimMontage>(Animation))
+			{
+				if (MeshComp->GetAnimInstance()->Montage_IsPlaying(CurPlayingMontage))
+				{
+					if (Human->ARPG_AnyInputIsPressed(ARPG_InputType::ToBitMask(EARPG_InputType::LeftLightAttack) | ARPG_InputType::ToBitMask(EARPG_InputType::LeftHeavyAttack)))
+					{
+						if (Human->LeftWeapon)
+						{
+							Human->LeftWeapon->AttackAnimSet->InvokePlayDodgeAnim(Human, DodgeDirection);
+						}
+					}
+					else if (Human->ARPG_AnyInputIsPressed(ARPG_InputType::ToBitMask(EARPG_InputType::RightLightAttack) | ARPG_InputType::ToBitMask(EARPG_InputType::RightHeavyAttack)))
+					{
+						if (Human->RightWeapon)
+						{
+							Human->RightWeapon->AttackAnimSet->InvokePlayDodgeAnim(Human, DodgeDirection);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+FString UARPG_DodgeAttack::GetNotifyName_Implementation() const
+{
+	return TEXT("闪避攻击");
+}
+
+void UARPG_Human_TakeWeaponPos::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (bPullOutWeapon)
+		{
+			Human->LetTheWeaponInHand();
+		}
+		else
+		{
+			Human->LetTheWeaponInWeaponBack();
+		}
+	}
+}
+
+FString UARPG_Human_TakeWeaponPos::GetNotifyName_Implementation() const
+{
+	return TEXT("武器位置");
+}
+
+void UARPG_Human_WeaponTrace::NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (AARPG_WeaponBase* Weapon = bIsLeftWeapon ? Human->LeftWeapon : Human->RightWeapon)
+		{
+			Weapon->ReceiveDamageAction = ReceiveDamageAction;
+			Weapon->AnimAddHitStunValue = AddHitStunValue;
+			Weapon->SetEnableNearAttackTrace(true);
+		}
+	}
+}
+
+void UARPG_Human_WeaponTrace::NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (AARPG_WeaponBase* Weapon = bIsLeftWeapon ? Human->LeftWeapon : Human->RightWeapon)
+		{
+			Weapon->ReceiveDamageAction = nullptr;
+			Weapon->SetEnableNearAttackTrace(false);
+		}
+	}
+}
+
+FString UARPG_Human_WeaponTrace::GetNotifyName_Implementation() const
+{
+	return TEXT("武器伤害检测");
+}
+
+void UARPG_Human_FallingAttackTrace::NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (AARPG_WeaponBase* Weapon = bIsLeftWeapon ? Human->LeftWeapon : Human->RightWeapon)
+		{
+			Weapon->ReceiveDamageAction = ReceiveDamageAction;
+			Weapon->AnimAddHitStunValue = AddHitStunValue;
+			Weapon->SetEnableFallingAttackTrace(true, bClearIgnoreList);
+		}
+	}
+}
+
+void UARPG_Human_FallingAttackTrace::NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (AARPG_WeaponBase* Weapon = bIsLeftWeapon ? Human->LeftWeapon : Human->RightWeapon)
+		{
+			Weapon->SetEnableFallingAttackTrace(false, bClearIgnoreList);
+		}
+	}
+}
+
+void UARPG_Human_PullOutArrow::NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (AARPG_WeaponBase* Weapon = bIsLeftWeapon ? Human->LeftWeapon : Human->RightWeapon)
+		{
+			if (AARPG_BowBase* Bow = Cast<AARPG_BowBase>(Weapon))
+			{
+				Bow->SpawnArrowInHand();
+				Bow->HoldingTime = 0.f;
+			}
+		}
+	}
+}
+
+void UARPG_Human_PullOutArrow::NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (AARPG_WeaponBase* Weapon = bIsLeftWeapon ? Human->LeftWeapon : Human->RightWeapon)
+		{
+			if (AARPG_BowBase* Bow = Cast<AARPG_BowBase>(Weapon))
+			{
+				if (Bow->HoldingArrow)
+				{
+					Bow->HoldingArrow->Destroy();
+					Bow->HoldingArrow = nullptr;
+				}
+			}
+		}
+	}
+}
+
+void UARPG_Human_PullBow::NotifyTick(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float FrameDeltaTime)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (AARPG_WeaponBase* Weapon = bIsLeftWeapon ? Human->LeftWeapon : Human->RightWeapon)
+		{
+			if (AARPG_BowBase* Bow = Cast<AARPG_BowBase>(Weapon))
+			{
+				Bow->HoldingTime += FrameDeltaTime;
+			}
+		}
+	}
+}
+
+void UARPG_Human_LaunchArrow::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	if (AHumanBase* Human = Cast<AHumanBase>(MeshComp->GetOwner()))
+	{
+		if (AARPG_WeaponBase* Weapon = bIsLeftWeapon ? Human->LeftWeapon : Human->RightWeapon)
+		{
+			if (AARPG_BowBase* Bow = Cast<AARPG_BowBase>(Weapon))
+			{
+				Bow->LaunchArrow(FullBowTime);
+			}
+		}
+	}
+}
+
+
+
---
+++ Source/ARPG/Public/CharacterUtil/Human_AnimNotify.h
@@ -0,0 +1,138 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "CharacterType.h"
+#include <SubclassOf.h>
+#include "Human_AnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (DisplayName = "输入_闪避攻击"))
+class ARPG_API UARPG_DodgeAttack : public UAnimNotifyState
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "动画")
+	EDodgeDirection DodgeDirection;
+
+	virtual void NotifyTick(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float FrameDeltaTime) override;
+
+	virtual FString GetNotifyName_Implementation() const override;
+};
+
+UCLASS(meta = (DisplayName = "人类_武器位置"))
+class ARPG_API UARPG_Human_TakeWeaponPos : public UAnimNotify
+{
+	GENERATED_BODY()
+public:
+	UARPG_Human_TakeWeaponPos()
+	{
+		bIsNativeBranchingPoint = true;
+	}
+
+	UPROPERTY(EditAnywhere, Category = "武器位置", meta = (DisplayName = "为左手武器"))
+	uint8 bPullOutWeapon : 1;
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+
+	virtual FString GetNotifyName_Implementation() const override;
+};
+
+UCLASS(meta = (DisplayName = "人类_武器伤害检测"))
+class ARPG_API UARPG_Human_WeaponTrace : public UAnimNotifyState
+{
+	GENERATED_BODY()
+public:
+	UARPG_Human_WeaponTrace()
+		:bClearIgnoreList(true)
+	{}
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "为左手武器"))
+	uint8 bIsLeftWeapon : 1;
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "清空攻击到对象列表"))
+	uint8 bClearIgnoreList : 1;
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "特殊受击动作"))
+	TSubclassOf<class UReceiveDamageActionBase> ReceiveDamageAction;
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "削韧增加量"))
+	float AddHitStunValue = 0.f;
+
+	virtual void NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration) override;
+	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation) override;
+
+	virtual FString GetNotifyName_Implementation() const override;
+};
+
+UCLASS(meta = (DisplayName = "人类_下落攻击检测"))
+class ARPG_API UARPG_Human_FallingAttackTrace : public UAnimNotifyState
+{
+	GENERATED_BODY()
+public:
+	UARPG_Human_FallingAttackTrace()
+		:bClearIgnoreList(true)
+	{}
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "为左手武器"))
+	uint8 bIsLeftWeapon : 1;
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "清空攻击到对象列表"))
+	uint8 bClearIgnoreList : 1;
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "特殊受击动作"))
+	TSubclassOf<class UReceiveDamageActionBase> ReceiveDamageAction;
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "削韧增加量"))
+	float AddHitStunValue = 0.f;
+
+	virtual void NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration) override;
+	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation) override;
+};
+
+UCLASS(meta = (DisplayName = "人类_拔出箭"))
+class ARPG_API UARPG_Human_PullOutArrow : public UAnimNotifyState
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "为左手武器"))
+	uint8 bIsLeftWeapon : 1;
+
+	virtual void NotifyBegin(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float TotalDuration) override;
+	virtual void NotifyEnd(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation) override;
+};
+
+UCLASS(meta = (DisplayName = "人类_拉弓"))
+class ARPG_API UARPG_Human_PullBow : public UAnimNotifyState
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "为左手武器"))
+	uint8 bIsLeftWeapon : 1;
+
+	virtual void NotifyTick(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation, float FrameDeltaTime) override;
+};
+
+UCLASS(meta = (DisplayName = "人类_射出箭"))
+class ARPG_API UARPG_Human_LaunchArrow : public UAnimNotify
+{
+	GENERATED_BODY()
+public:
+	UARPG_Human_LaunchArrow()
+	{
+		bIsNativeBranchingPoint = true;
+	}
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "为左手武器"))
+	uint8 bIsLeftWeapon : 1;
+
+	UPROPERTY(EditAnywhere, Category = "攻击", meta = (DisplayName = "拉满弓所需时间"))
+	float FullBowTime = 2.f;
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+};
