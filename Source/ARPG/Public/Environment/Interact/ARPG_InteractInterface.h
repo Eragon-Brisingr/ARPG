cc57c7243a8e9189d26287fc0a8286348ec0b2c0
Eragon <450614754@qq.com>
2018-11-06 21:32:56 +0800

    交互行为接口

---
+++ Source/ARPG/Public/Environment/Interact/ARPG_InteractInterface.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ARPG_InteractInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UARPG_InteractInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class ARPG_API IARPG_InteractInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	//申请交互 e.g.移动至交互位置
+	UFUNCTION(BlueprintNativeEvent, Category = "游戏|交互")
+	void WhenInvokeInteract(class ACharacterBase* InteractInvoker);
+	virtual void WhenInvokeInteract_Implementation(class ACharacterBase* InteractInvoker);
+	static void WhenInvokeInteract(UObject* Obj, class ACharacterBase* InteractInvoker);
+
+	//执行交互 e.g.道具添加入背包
+	UFUNCTION(BlueprintNativeEvent, Category = "游戏|交互")
+	void WhenExecuteInteract(class ACharacterBase* InteractInvoker);
+	virtual void WhenExecuteInteract_Implementation(class ACharacterBase* InteractInvoker) {}
+	static void WhenExecuteInteract(UObject* Obj, class ACharacterBase* InteractInvoker) { IARPG_InteractInterface::Execute_WhenExecuteInteract(Obj, InteractInvoker); }
+
+	UFUNCTION(BlueprintNativeEvent, Category = "环境|交互")
+	bool CanInteract(const class ACharacterBase* InteractInvoker) const;
+	virtual bool CanInteract_Implementation(const class ACharacterBase* InteractInvoker) const { return false; }
+	static bool CanInteract(UObject* Obj, const class ACharacterBase* InteractInvoker) { return IARPG_InteractInterface::Execute_CanInteract(Obj, InteractInvoker); }
+public:
+	UFUNCTION(BlueprintNativeEvent, Category = "游戏|交互")
+	void GetHintInfo(class ACharacterBase* InteractInvoker);
+	virtual void GetHintInfo_Implementation(class ACharacterBase* InteractInvoker) {}
+};
