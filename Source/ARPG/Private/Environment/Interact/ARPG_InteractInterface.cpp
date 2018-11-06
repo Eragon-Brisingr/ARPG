2499d6dbc0c1cd5e3b7483001a3af220814dd6f4
Eragon <450614754@qq.com>
2018-11-06 21:32:56 +0800

    交互行为接口
    Former-commit-id: cc57c7243a8e9189d26287fc0a8286348ec0b2c0

---
+++ Source/ARPG/Private/Environment/Interact/ARPG_InteractInterface.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ARPG_InteractInterface.h"
+
+
+// Add default functionality here for any IARPG_InteractInterface functions that are not pure virtual.
+bool IsOverrideWhenInvokeInteract;
+
+void IARPG_InteractInterface::WhenInvokeInteract_Implementation(class ACharacterBase* InteractInvoker)
+{
+	IsOverrideWhenInvokeInteract = false;
+}
+
+void IARPG_InteractInterface::WhenInvokeInteract(UObject* Obj, class ACharacterBase* InteractInvoker)
+{
+	IsOverrideWhenInvokeInteract = true;
+	IARPG_InteractInterface::Execute_WhenInvokeInteract(Obj, InteractInvoker);
+	if (IsOverrideWhenInvokeInteract == false)
+	{
+		WhenExecuteInteract(Obj, InteractInvoker);
+	}
+}
