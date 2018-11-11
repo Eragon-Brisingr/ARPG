d15a33d0cff46d3c8b2b412d5437a9e1316828c0
Eragon <450614754@qq.com>
2018-11-11 22:24:33 +0800

    添加AI模块

---
+++ Source/ARPG/Public/AI/ARPG_AIControllerBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ARPG_AIControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API AARPG_AIControllerBase : public AAIController
+{
+	GENERATED_BODY()
+	
+};
