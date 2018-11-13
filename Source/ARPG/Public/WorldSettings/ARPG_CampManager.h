968e35431b2bae687d08e13e41bc33aaa1645e63
Eragon <450614754@qq.com>
2018-11-13 20:28:48 +0800

    引入阵营系统

---
+++ Source/ARPG/Public/WorldSettings/ARPG_CampManager.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TextProperty.h"
+#include "XD_CampManager.h"
+#include "ARPG_CampManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UARPG_CampManager : public UXD_CampManager
+{
+	GENERATED_BODY()
+	
+};
