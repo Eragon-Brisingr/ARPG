77f7c887176f8643c22bc36668a1eb19002f60e1
Eragon <450614754@qq.com>
2018-11-15 22:01:38 +0800

    拓展AI相关函数
    Former-commit-id: c66b7b4106235cd1bca1d5143dc46eab363ecc55

---
+++ Source/ARPG/Private/AI/Utils/ARPG_AI_ExFunctionLibrary.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ARPG_AI_ExFunctionLibrary.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISense_Sight.h"
+
+class UAISenseConfig* UARPG_AI_ExFunctionLibrary::GetSenseConfig(class UAIPerceptionComponent* AIPerceptionComponent, TSubclassOf<class UAISense> AISenseType)
+{
+	if (AIPerceptionComponent)
+	{
+		FAISenseID Id = UAISense::GetSenseID(AISenseType);
+		if (Id.IsValid())
+		{
+			return AIPerceptionComponent->GetSenseConfig(Id);
+		}
+	}
+	return nullptr;
+}
