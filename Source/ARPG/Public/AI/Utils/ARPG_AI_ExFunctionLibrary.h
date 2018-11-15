c66b7b4106235cd1bca1d5143dc46eab363ecc55
Eragon <450614754@qq.com>
2018-11-15 22:01:38 +0800

    拓展AI相关函数

---
+++ Source/ARPG/Public/AI/Utils/ARPG_AI_ExFunctionLibrary.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SubclassOf.h"
+#include "ARPG_AI_ExFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UARPG_AI_ExFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	//修改完别忘记调用RequestStimuliListenerUpdate
+	UFUNCTION(BlueprintCallable, Category = "AI|Perception")
+	static class UAISenseConfig* GetSenseConfig(class UAIPerceptionComponent* AIPerceptionComponent, TSubclassOf<class UAISense> AISenseType);
+};
