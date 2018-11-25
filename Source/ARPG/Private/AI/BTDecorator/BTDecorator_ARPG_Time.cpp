92080addf364e05af04a89d4a3f48ac27502b0ad
Eragon <eragon.xuduo@gmail.com>
2018-11-25 21:57:00 +0800

    提交时间相关BTD
    Former-commit-id: 38e546d1fc89d315b69833d712267f9d0758ea2a

---
+++ Source/ARPG/Private/AI/BTDecorator/BTDecorator_ARPG_Time.cpp
@@ -0,0 +1,79 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTDecorator_ARPG_Time.h"
+#include "ARPG_TimeManager.h"
+#include "BlueprintNodeHelpers.h"
+
+FString UBTDecorator_ARPG_TimeBase::GetStaticDescription() const
+{
+	FString ReturnDesc = Super::GetStaticDescription();
+
+	UBTDecorator_ARPG_TimeBase* CDO = (UBTDecorator_ARPG_TimeBase*)(GetClass()->GetDefaultObject());
+	if (CDO)
+	{
+		TArray<UProperty*> PropertyData;
+		UClass* StopAtClass = UBTDecorator_ARPG_TimeBase::StaticClass();
+		FString PropertyDesc = BlueprintNodeHelpers::CollectPropertyDescription(this, StopAtClass, PropertyData);
+		if (PropertyDesc.Len())
+		{
+			ReturnDesc += TEXT(":\n\n");
+			ReturnDesc += PropertyDesc;
+		}
+	}
+
+	return ReturnDesc;
+}
+
+bool UBTDecorator_ARPG_Time_InHourRange::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (UXD_TimeManager* TimeManager = UARPG_TimeManager::GetGameTimeManager(&OwnerComp))
+	{
+		return TimeManager->CurrentTime.InHourRange(StartTime, EndTime);
+	}
+	return false;
+}
+
+bool UBTDecorator_ARPG_Time_InDayRange::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (UXD_TimeManager* TimeManager = UARPG_TimeManager::GetGameTimeManager(&OwnerComp))
+	{
+		return TimeManager->CurrentTime.InDayRange(StartTime, EndTime);
+	}
+	return false;
+}
+
+bool UBTDecorator_ARPG_Time_InWeekRange::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (UXD_TimeManager* TimeManager = UARPG_TimeManager::GetGameTimeManager(&OwnerComp))
+	{
+		return TimeManager->CurrentTime.InWeekRange(StartTime, EndTime);
+	}
+	return false;
+}
+
+bool UBTDecorator_ARPG_Time_InMonthRange::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (UXD_TimeManager* TimeManager = UARPG_TimeManager::GetGameTimeManager(&OwnerComp))
+	{
+		return TimeManager->CurrentTime.InMonthRange(StartTime, EndTime);
+	}
+	return false;
+}
+
+bool UBTDecorator_ARPG_Time_InYearRange::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (UXD_TimeManager* TimeManager = UARPG_TimeManager::GetGameTimeManager(&OwnerComp))
+	{
+		return TimeManager->CurrentTime.InYearRange(StartTime, EndTime);
+	}
+	return false;
+}
+
+bool UBTDecorator_ARPG_Time_InSpecialRange::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	if (UXD_TimeManager* TimeManager = UARPG_TimeManager::GetGameTimeManager(&OwnerComp))
+	{
+		return TimeManager->CurrentTime.InSpecialTimeRange(StartTime, EndTime);
+	}
+	return false;
+}
---
+++ Source/ARPG/Public/AI/BTDecorator/BTDecorator_ARPG_Time.h
@@ -0,0 +1,113 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "XD_TimeSystemType.h"
+#include "BTDecorator_ARPG_Time.generated.h"
+
+/**
+ * 
+ */
+UCLASS(abstract)
+class ARPG_API UBTDecorator_ARPG_TimeBase : public UBTDecorator
+{
+	GENERATED_BODY()
+public:
+
+private:
+	virtual FString GetStaticDescription() const override;
+};
+
+UCLASS()
+class ARPG_API UBTDecorator_ARPG_Time_InHourRange : public UBTDecorator_ARPG_TimeBase
+{
+	GENERATED_BODY()
+public:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryHourConfig StartTime;
+
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryHourConfig EndTime;
+};
+
+UCLASS()
+class ARPG_API UBTDecorator_ARPG_Time_InDayRange : public UBTDecorator_ARPG_TimeBase
+{
+	GENERATED_BODY()
+public:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryDayConfig StartTime;
+
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryDayConfig EndTime;
+};
+
+UCLASS()
+class ARPG_API UBTDecorator_ARPG_Time_InWeekRange : public UBTDecorator_ARPG_TimeBase
+{
+	GENERATED_BODY()
+public:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryWeekConfig StartTime;
+
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryWeekConfig EndTime;
+};
+
+UCLASS()
+class ARPG_API UBTDecorator_ARPG_Time_InMonthRange : public UBTDecorator_ARPG_TimeBase
+{
+	GENERATED_BODY()
+public:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryMonthConfig StartTime;
+
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryMonthConfig EndTime;
+};
+
+UCLASS()
+class ARPG_API UBTDecorator_ARPG_Time_InYearRange : public UBTDecorator_ARPG_TimeBase
+{
+	GENERATED_BODY()
+public:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryYearConfig StartTime;
+
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_EveryYearConfig EndTime;
+};
+
+UCLASS()
+class ARPG_API UBTDecorator_ARPG_Time_InSpecialRange : public UBTDecorator_ARPG_TimeBase
+{
+	GENERATED_BODY()
+public:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+public:
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_SpecialTimeConfig StartTime;
+
+	UPROPERTY(EditAnywhere, Category = "时间")
+	FXD_SpecialTimeConfig EndTime;
+};
+
+
