457c6f7d4a50735ade10176a1c6022973b99a405
Eragon <450614754@qq.com>
2018-12-01 18:37:13 +0800

    行为的容器类型制作中

---
+++ Source/ARPG/Private/CharacterUtil/Behavior/ARPG_CharacterBehaviorContainer.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ARPG_CharacterBehaviorContainer.h"
+
+UCBC_RandomSelect::UCBC_RandomSelect()
+{
+	BehaviorType = UCB_RandomSelect::StaticClass();
+}
+
+void UCB_RandomSelect::ExecuteBehavior(class ACharacterBase* Executer, const FVector& Location, const FRotator& Rotation)
+{
+
+}
+
+void UCB_RandomSelect::AbortBehavior(class ACharacterBase* Executer)
+{
+
+}
+
+const UCBC_RandomSelect* UCB_RandomSelect::GetConfig() const
+{
+	return Cast<UCBC_RandomSelect>(Config);
+}
---
+++ Source/ARPG/Public/CharacterUtil/Behavior/ARPG_CharacterBehaviorContainer.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ARPG_CharacterBehaviorBase.h"
+#include "ARPG_CharacterBehaviorContainer.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (DisplayName = "行为容器_随机选择"))
+class ARPG_API UCBC_RandomSelect : public UARPG_CharacterBehaviorConfigBase
+{
+	GENERATED_BODY()
+public:
+	UCBC_RandomSelect();
+
+	UPROPERTY(EditAnywhere, Category = "容器")
+	TMap<class UARPG_CharacterBehaviorConfigBase*, float> RandomMap;
+};
+
+UCLASS()
+class ARPG_API UCB_RandomSelect : public UARPG_CharacterBehaviorBase
+{
+	GENERATED_BODY()
+public:
+	void ExecuteBehavior(class ACharacterBase* Executer, const FVector& Location, const FRotator& Rotation) override;
+
+	void AbortBehavior(class ACharacterBase* Executer) override;
+public:
+	const UCBC_RandomSelect* GetConfig() const;
+};
