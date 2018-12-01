27771685064c9ade62084b58e7fb68805fc2803c
Eragon <eragon.xuduo@gmail.com>
2018-12-01 23:28:15 +0800

    添加[行为容器_随机选择]
    Former-commit-id: c9db1f30caffd54c97a7186f767ded00f79653df

--- Source/ARPG/Private/CharacterUtil/Behavior/ARPG_CharacterBehaviorContainer.cpp
+++ Source/ARPG/Private/CharacterUtil/Behavior/ARPG_CharacterBehaviorContainer.cpp
@@ -9,12 +9,45 @@ UCBC_RandomSelect::UCBC_RandomSelect()
 
 void UCB_RandomSelect::ExecuteBehavior(class ACharacterBase* Executer, const FVector& Location, const FRotator& Rotation)
 {
+	CurrentBehavior = nullptr;
 
+	float TotalWeight = 0;
+	for (const FRandomSelectBehaviorElement& Element : GetConfig()->RandomBehaviors)
+	{
+		TotalWeight += Element.Weight;
+	}
+
+	float RandomRes = FMath::FRand() * TotalWeight;
+	for (const FRandomSelectBehaviorElement& Element : GetConfig()->RandomBehaviors)
+	{
+		RandomRes -= Element.Weight;
+		if (RandomRes < 0.f)
+		{
+			CurrentBehavior = Element.Behavior;
+			break;
+		}
+	}
+
+	if (CurrentBehavior)
+	{
+		CurrentBehavior->ExecuteBehavior(Executer, Location, Rotation, OnBehaviorFinished);
+	}
+	else
+	{
+		FinishExecute(true);
+	}
 }
 
 void UCB_RandomSelect::AbortBehavior(class ACharacterBase* Executer)
 {
-
+	if (CurrentBehavior)
+	{
+		CurrentBehavior->AbortBehavior(Executer, OnBehaviorAbortFinished);
+	}
+	else
+	{
+		FinishAbort();
+	}
 }
 
 const UCBC_RandomSelect* UCB_RandomSelect::GetConfig() const
