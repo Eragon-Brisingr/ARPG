ea14f1bf638d69efd1408956c0b39616e1355030
Eragon <450614754@qq.com>
2018-12-03 20:52:10 +0800

    添加CharacterBehavior的BTTask包装节点，Note：没考虑WorldOriginOffset
    Former-commit-id: 339492ecce49927d021e797f36c4f2d413574e61

---
+++ Source/ARPG/Public/AI/BTTask/BTTask_CharacterBehavior.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_CharacterBehavior.generated.h"
+
+class AActor;
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UBTTask_CharacterBehavior : public UBTTaskNode
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "行为", Instanced)
+	class UARPG_CharacterBehaviorConfigBase* Behavior;
+
+	UPROPERTY(EditAnywhere, Category = "行为")
+	FVector Location;
+
+	UPROPERTY(EditAnywhere, Category = "行为")
+	FRotator Rotation;
+
+#if WITH_EDITORONLY_DATA
+	UPROPERTY(EditAnywhere, Category = "行为")
+	TSoftObjectPtr<AActor> Target;
+
+	UPROPERTY(EditAnywhere, Category = "行为")
+	uint8 bRefresh : 1;
+
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	void WhenBehaviorFinished(bool Succeed, UBehaviorTreeComponent* OwnerComp);
+	void WhenBehaviorAbortFinished(UBehaviorTreeComponent* OwnerComp);
+};
