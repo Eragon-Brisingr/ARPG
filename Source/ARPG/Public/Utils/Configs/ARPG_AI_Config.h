6732c2d81c7c00d2ec5980dfec56a4e8fc40d2b3
Eragon <450614754@qq.com>
2018-12-29 15:31:11 +0800

    添加AI可配置项

---
+++ Source/ARPG/Public/Utils/Configs/ARPG_AI_Config.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ARPG_AI_Config.generated.h"
+
+class UCurveFloat;
+
+/**
+ * 
+ */
+UCLASS(Config = "ARPG_AI_Config", defaultconfig)
+class ARPG_API UARPG_AI_Config : public UObject
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, Category = "AI", Config)
+	TSoftObjectPtr<UCurveFloat> DefaultSightAlertCurve;
+
+	UPROPERTY(EditAnywhere, Category = "AI", Config)
+	TSoftObjectPtr<UCurveFloat> DefaultHearAlertCurve;
+
+	UPROPERTY(EditAnywhere, Category = "AI", Config)
+	TSoftObjectPtr<UCurveFloat> DefaultNoseAlertCurve;
+};
