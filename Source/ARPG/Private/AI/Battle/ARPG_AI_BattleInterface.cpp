628ec9b3bf96b4bbdd5b89b1bf9f73e56369ffa2
Eragon_Brisingr <450614754@qq.com>
2018-12-30 21:55:09 +0800

    添加战斗接口中防御的部分
    Former-commit-id: 00983f89b8a6571da911163143bb4f457fcdc612

--- Source/ARPG/Private/AI/Battle/ARPG_AI_BattleInterface.cpp
+++ Source/ARPG/Private/AI/Battle/ARPG_AI_BattleInterface.cpp
@@ -34,12 +34,98 @@ void IARPG_AI_BattleInterface::InvokeAttack_Implementation(class AActor* AttackT
 	OnAttackFinished.ExecuteIfBound(false);
 }
 
+void IARPG_AI_BattleInterface::AbortAttack_Implementation(class AActor* AttackTarget, const FBP_OnAttackAborted& OnAttackAborted)
+{
+	OnAttackAborted.ExecuteIfBound();
+}
+
 void IARPG_AI_BattleInterface::AttackingTick_Implementation(class AActor* AttackTarget, float DeltaSecond)
 {
 
 }
 
-void IARPG_AI_BattleInterface::AbortAttack_Implementation(class AActor* AttackTarget, const FBP_OnAttackAborted& OnAttackAborted)
+bool IARPG_AI_BattleInterface::CanDefense_Implementation(class AActor* DefenseTarget) const
 {
-	OnAttackAborted.ExecuteIfBound();
+	return false;
+}
+
+FVector IARPG_AI_BattleInterface::GetDefenseMoveLocation_Implementation(class AActor* DefenseTarget) const
+{
+	if (const AActor* Owner = Cast<AActor>(this))
+	{
+		return Owner->GetActorLocation();
+	}
+	return FVector::ZeroVector;
+}
+
+FRotator IARPG_AI_BattleInterface::GetDefenseFaceRotation_Implementation(class AActor* DefenseTarget) const
+{
+	if (const AActor* Owner = Cast<AActor>(this))
+	{
+		return (DefenseTarget->GetActorLocation() - Owner->GetActorLocation()).Rotation();
+	}
+	return FRotator::ZeroRotator;
+}
+
+bool IARPG_AI_BattleInterface::IsAllowedDefense_Implementation(class AActor* DefenseTarget) const
+{
+	return false;
+}
+
+void IARPG_AI_BattleInterface::InvokeDefense_Implementation(class AActor* DefenseTarget, const FBP_OnDefenseFinished& OnDefenseFinished)
+{
+	OnDefenseFinished.ExecuteIfBound(false);
+}
+
+void IARPG_AI_BattleInterface::AbortDefense_Implementation(class AActor* DefenseTarget, const FBP_OnDefenseAborted& OnDefenseAborted)
+{
+	OnDefenseAborted.ExecuteIfBound();
+}
+
+void IARPG_AI_BattleInterface::DefenseingTick_Implementation(class AActor* DefenseTarget, float DeltaSecond)
+{
+
+}
+
+bool IARPG_AI_BattleInterface::CanDefenseSwipe_Implementation(class AActor* DefenseSwipeTarget) const
+{
+	return false;
+}
+
+FVector IARPG_AI_BattleInterface::GetDefenseSwipeMoveLocation_Implementation(class AActor* DefenseSwipeTarget) const
+{
+	if (const AActor* Owner = Cast<AActor>(this))
+	{
+		return Owner->GetActorLocation();
+	}
+	return FVector::ZeroVector;
+}
+
+FRotator IARPG_AI_BattleInterface::GetDefenseSwipeFaceRotation_Implementation(class AActor* DefenseSwipeTarget) const
+{
+	if (const AActor* Owner = Cast<AActor>(this))
+	{
+		return (DefenseSwipeTarget->GetActorLocation() - Owner->GetActorLocation()).Rotation();
+	}
+	return FRotator::ZeroRotator;
+}
+
+bool IARPG_AI_BattleInterface::IsAllowedDefenseSwipe_Implementation(class AActor* DefenseSwipeTarget) const
+{
+	return false;
+}
+
+void IARPG_AI_BattleInterface::InvokeDefenseSwipe_Implementation(class AActor* DefenseSwipeTarget, const FBP_OnDefenseSwipeFinished& OnDefenseSwipeFinished)
+{
+	OnDefenseSwipeFinished.ExecuteIfBound(false);
+}
+
+void IARPG_AI_BattleInterface::AbortDefenseSwipe_Implementation(class AActor* DefenseSwipeTarget, const FBP_OnDefenseSwipeAborted& OnDefenseSwipeAborted)
+{
+	OnDefenseSwipeAborted.ExecuteIfBound();
+}
+
+void IARPG_AI_BattleInterface::DefenseSwipeingTick_Implementation(class AActor* DefenseSwipeTarget, float DeltaSecond)
+{
+
 }
--- Source/ARPG/Public/AI/Battle/ARPG_AI_BattleInterface.h
+++ Source/ARPG/Public/AI/Battle/ARPG_AI_BattleInterface.h
@@ -41,6 +41,74 @@ public:
 	void ExecuteIfBound() const { OnAttackAborted.ExecuteIfBound(); }
 };
 
+DECLARE_DELEGATE_OneParam(FARPG_OnDefenseFinished, bool /*Succeed*/);
+
+USTRUCT(BlueprintType, meta = (BlueprintInternalUseOnly = true))
+struct ARPG_API FBP_OnDefenseFinished
+{
+	GENERATED_BODY()
+public:
+	FBP_OnDefenseFinished() = default;
+	FBP_OnDefenseFinished(const FARPG_OnDefenseFinished& OnDefenseFinished)
+		:OnDefenseFinished(OnDefenseFinished)
+	{}
+
+	FARPG_OnDefenseFinished OnDefenseFinished;
+
+	void ExecuteIfBound(bool Succeed) const { OnDefenseFinished.ExecuteIfBound(Succeed); }
+};
+
+DECLARE_DELEGATE(FARPG_OnDefenseAborted);
+
+USTRUCT(BlueprintType, meta = (BlueprintInternalUseOnly = true))
+struct ARPG_API FBP_OnDefenseAborted
+{
+	GENERATED_BODY()
+public:
+	FBP_OnDefenseAborted() = default;
+	FBP_OnDefenseAborted(const FARPG_OnDefenseAborted& OnDefenseAborted)
+		:OnDefenseAborted(OnDefenseAborted)
+	{}
+
+	FARPG_OnDefenseAborted OnDefenseAborted;
+
+	void ExecuteIfBound() const { OnDefenseAborted.ExecuteIfBound(); }
+};
+
+DECLARE_DELEGATE_OneParam(FARPG_OnDefenseSwipeFinished, bool /*Succeed*/);
+
+USTRUCT(BlueprintType, meta = (BlueprintInternalUseOnly = true))
+struct ARPG_API FBP_OnDefenseSwipeFinished
+{
+	GENERATED_BODY()
+public:
+	FBP_OnDefenseSwipeFinished() = default;
+	FBP_OnDefenseSwipeFinished(const FARPG_OnDefenseSwipeFinished& OnDefenseSwipeFinished)
+		:OnDefenseSwipeFinished(OnDefenseSwipeFinished)
+	{}
+
+	FARPG_OnDefenseSwipeFinished OnDefenseSwipeFinished;
+
+	void ExecuteIfBound(bool Succeed) const { OnDefenseSwipeFinished.ExecuteIfBound(Succeed); }
+};
+
+DECLARE_DELEGATE(FARPG_OnDefenseSwipeAborted);
+
+USTRUCT(BlueprintType, meta = (BlueprintInternalUseOnly = true))
+struct ARPG_API FBP_OnDefenseSwipeAborted
+{
+	GENERATED_BODY()
+public:
+	FBP_OnDefenseSwipeAborted() = default;
+	FBP_OnDefenseSwipeAborted(const FARPG_OnDefenseSwipeAborted& OnDefenseSwipeAborted)
+		:OnDefenseSwipeAborted(OnDefenseSwipeAborted)
+	{}
+
+	FARPG_OnDefenseSwipeAborted OnDefenseSwipeAborted;
+
+	void ExecuteIfBound() const { OnDefenseSwipeAborted.ExecuteIfBound(); }
+};
+
 
 // This class does not need to be modified.
 UINTERFACE(MinimalAPI)
@@ -56,6 +124,8 @@ class ARPG_API IARPG_AI_BattleInterface
 	GENERATED_BODY()
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+
+	//Attack
 public:
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
 	bool CanAttack(class AActor* AttackTarget) const;
@@ -91,6 +161,80 @@ public:
 	void AttackingTick(class AActor* AttackTarget, float DeltaSecond);
 	virtual void AttackingTick_Implementation(class AActor* AttackTarget, float DeltaSecond);
 	static void AttackingTick(UObject* Obj, class AActor* AttackTarget, float DeltaSecond) { IARPG_AI_BattleInterface::Execute_AttackingTick(Obj, AttackTarget, DeltaSecond); }
+
+
+	//Defense
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	bool CanDefense(class AActor* DefenseTarget) const;
+	virtual bool CanDefense_Implementation(class AActor* DefenseTarget) const;
+	static bool CanDefense(const UObject* Obj, class AActor* DefenseTarget) { return IARPG_AI_BattleInterface::Execute_CanDefense(Obj, DefenseTarget); }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, BlueprintNativeEvent, Category = "战斗")
+	FVector GetDefenseMoveLocation(class AActor* DefenseTarget) const;
+	virtual FVector GetDefenseMoveLocation_Implementation(class AActor* DefenseTarget) const;
+	static FVector GetDefenseMoveLocation(const UObject* Obj, class AActor* DefenseTarget) { return IARPG_AI_BattleInterface::Execute_GetDefenseMoveLocation(Obj, DefenseTarget); }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, BlueprintNativeEvent, Category = "战斗")
+	FRotator GetDefenseFaceRotation(class AActor* DefenseTarget) const;
+	virtual FRotator GetDefenseFaceRotation_Implementation(class AActor* DefenseTarget) const;
+	static FRotator GetDefenseFaceRotation(const UObject* Obj, class AActor* DefenseTarget) { return IARPG_AI_BattleInterface::Execute_GetDefenseFaceRotation(Obj, DefenseTarget); }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	bool IsAllowedDefense(class AActor* DefenseTarget) const;
+	virtual bool IsAllowedDefense_Implementation(class AActor* DefenseTarget) const;
+	static bool IsAllowedDefense(UObject* Obj, class AActor* DefenseTarget) { return IARPG_AI_BattleInterface::Execute_IsAllowedDefense(Obj, DefenseTarget); }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	void InvokeDefense(class AActor* DefenseTarget, const FBP_OnDefenseFinished& OnDefenseFinished);
+	virtual void InvokeDefense_Implementation(class AActor* DefenseTarget, const FBP_OnDefenseFinished& OnDefenseFinished);
+	static void InvokeDefense(UObject* Obj, class AActor* DefenseTarget, const FBP_OnDefenseFinished& OnDefenseFinished) { IARPG_AI_BattleInterface::Execute_InvokeDefense(Obj, DefenseTarget, OnDefenseFinished); }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	void AbortDefense(class AActor* DefenseTarget, const FBP_OnDefenseAborted& OnDefenseAborted);
+	virtual void AbortDefense_Implementation(class AActor* DefenseTarget, const FBP_OnDefenseAborted& OnDefenseAborted);
+	static void AbortDefense(UObject* Obj, class AActor* DefenseTarget, const FBP_OnDefenseAborted& OnDefenseAborted) { IARPG_AI_BattleInterface::Execute_AbortDefense(Obj, DefenseTarget, OnDefenseAborted); }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	void DefenseingTick(class AActor* DefenseTarget, float DeltaSecond);
+	virtual void DefenseingTick_Implementation(class AActor* DefenseTarget, float DeltaSecond);
+	static void DefenseingTick(UObject* Obj, class AActor* DefenseTarget, float DeltaSecond) { IARPG_AI_BattleInterface::Execute_DefenseingTick(Obj, DefenseTarget, DeltaSecond); }
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	bool CanDefenseSwipe(class AActor* DefenseSwipeTarget) const;
+	virtual bool CanDefenseSwipe_Implementation(class AActor* DefenseSwipeTarget) const;
+	static bool CanDefenseSwipe(const UObject* Obj, class AActor* DefenseSwipeTarget) { return IARPG_AI_BattleInterface::Execute_CanDefenseSwipe(Obj, DefenseSwipeTarget); }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, BlueprintNativeEvent, Category = "战斗")
+	FVector GetDefenseSwipeMoveLocation(class AActor* DefenseSwipeTarget) const;
+	virtual FVector GetDefenseSwipeMoveLocation_Implementation(class AActor* DefenseSwipeTarget) const;
+	static FVector GetDefenseSwipeMoveLocation(const UObject* Obj, class AActor* DefenseSwipeTarget) { return IARPG_AI_BattleInterface::Execute_GetDefenseSwipeMoveLocation(Obj, DefenseSwipeTarget); }
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false, BlueprintNativeEvent, Category = "战斗")
+	FRotator GetDefenseSwipeFaceRotation(class AActor* DefenseSwipeTarget) const;
+	virtual FRotator GetDefenseSwipeFaceRotation_Implementation(class AActor* DefenseSwipeTarget) const;
+	static FRotator GetDefenseSwipeFaceRotation(const UObject* Obj, class AActor* DefenseSwipeTarget) { return IARPG_AI_BattleInterface::Execute_GetDefenseSwipeFaceRotation(Obj, DefenseSwipeTarget); }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	bool IsAllowedDefenseSwipe(class AActor* DefenseSwipeTarget) const;
+	virtual bool IsAllowedDefenseSwipe_Implementation(class AActor* DefenseSwipeTarget) const;
+	static bool IsAllowedDefenseSwipe(UObject* Obj, class AActor* DefenseSwipeTarget) { return IARPG_AI_BattleInterface::Execute_IsAllowedDefenseSwipe(Obj, DefenseSwipeTarget); }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	void InvokeDefenseSwipe(class AActor* DefenseSwipeTarget, const FBP_OnDefenseSwipeFinished& OnDefenseSwipeFinished);
+	virtual void InvokeDefenseSwipe_Implementation(class AActor* DefenseSwipeTarget, const FBP_OnDefenseSwipeFinished& OnDefenseSwipeFinished);
+	static void InvokeDefenseSwipe(UObject* Obj, class AActor* DefenseSwipeTarget, const FBP_OnDefenseSwipeFinished& OnDefenseSwipeFinished) { IARPG_AI_BattleInterface::Execute_InvokeDefenseSwipe(Obj, DefenseSwipeTarget, OnDefenseSwipeFinished); }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	void AbortDefenseSwipe(class AActor* DefenseSwipeTarget, const FBP_OnDefenseSwipeAborted& OnDefenseSwipeAborted);
+	virtual void AbortDefenseSwipe_Implementation(class AActor* DefenseSwipeTarget, const FBP_OnDefenseSwipeAborted& OnDefenseSwipeAborted);
+	static void AbortDefenseSwipe(UObject* Obj, class AActor* DefenseSwipeTarget, const FBP_OnDefenseSwipeAborted& OnDefenseSwipeAborted) { IARPG_AI_BattleInterface::Execute_AbortDefenseSwipe(Obj, DefenseSwipeTarget, OnDefenseSwipeAborted); }
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "战斗")
+	void DefenseSwipeingTick(class AActor* DefenseSwipeTarget, float DeltaSecond);
+	virtual void DefenseSwipeingTick_Implementation(class AActor* DefenseSwipeTarget, float DeltaSecond);
+	static void DefenseSwipeingTick(UObject* Obj, class AActor* DefenseSwipeTarget, float DeltaSecond) { IARPG_AI_BattleInterface::Execute_DefenseSwipeingTick(Obj, DefenseSwipeTarget, DeltaSecond); }
 };
 
 UCLASS()
@@ -103,4 +247,16 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "游戏|AI")
 	static void ExecuteAttackAborted(const FBP_OnAttackAborted& OnAttackFinished) { OnAttackFinished.ExecuteIfBound(); }
+
+	UFUNCTION(BlueprintCallable, Category = "游戏|AI")
+	static void ExecuteDefenseFinished(const FBP_OnDefenseFinished& OnDefenseFinished, bool Succeed = true) { OnDefenseFinished.ExecuteIfBound(Succeed); }
+
+	UFUNCTION(BlueprintCallable, Category = "游戏|AI")
+	static void ExecuteDefenseAborted(const FBP_OnDefenseAborted& OnDefenseFinished) { OnDefenseFinished.ExecuteIfBound(); }
+
+	UFUNCTION(BlueprintCallable, Category = "游戏|AI")
+	static void ExecuteDefenseSwipeFinished(const FBP_OnDefenseSwipeFinished& OnDefenseSwipeFinished, bool Succeed = true) { OnDefenseSwipeFinished.ExecuteIfBound(Succeed); }
+
+	UFUNCTION(BlueprintCallable, Category = "游戏|AI")
+	static void ExecuteDefenseSwipeAborted(const FBP_OnDefenseSwipeAborted& OnDefenseSwipeFinished) { OnDefenseSwipeFinished.ExecuteIfBound(); }
 };
