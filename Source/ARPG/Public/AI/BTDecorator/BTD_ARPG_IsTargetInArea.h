0c9c0cd3931932b77f0cfe828f99a53fe5e1757f
Eragon-Brisingr <450614754@qq.com>
2019-01-30 13:01:15 +0800

    同步子模块
    Former-commit-id: 32b9af782971ebe4d646718b856d78fd7185c69e

---
+++ Source/ARPG/Public/AI/BTDecorator/BTD_ARPG_IsTargetInArea.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Decorators/BTDecorator_BlackboardBase.h"
+#include "BTD_ARPG_IsTargetInArea.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UBTD_ARPG_IsTargetInArea : public UBTDecorator_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
