1ca09a33954eaea058e44d9b00d413386765701d
Eragon <450614754@qq.com>
2019-06-23 20:19:27 +0800

    修正客户端奔跑表现问题
    Former-commit-id: a984616e26f3a767a422cf7bc1d0a6fc538f8dd1

--- Source/ARPG/Private/Character/CharacterBase.cpp
+++ Source/ARPG/Private/Character/CharacterBase.cpp
@@ -123,10 +123,7 @@ void ACharacterBase::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	//TODO 挪入bIsLockingOther与bInvokeSprint的Setter驱动而不是轮询
-	if (IsPlayerControlled())
-	{
-		ARPG_MovementComponent->SetRotationMode(bIsLockingOther && ARPG_MovementComponent->bInvokeSprint == false ? ECharacterRotationMode::LookingDirection : ECharacterRotationMode::VelocityDirection);
-	}
+	ARPG_MovementComponent->SetRotationMode(bIsLockingOther && ARPG_MovementComponent->IsSprinting() == false ? ECharacterRotationMode::LookingDirection : ECharacterRotationMode::VelocityDirection);
 }
 
 // Called to bind functionality to input
