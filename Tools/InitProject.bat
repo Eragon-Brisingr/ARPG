873851f37d499e3a4128b6247a2701a080bf024f
Eragon <450614754@qq.com>
2019-11-09 13:34:32 +0800

    修改更新脚本和添加初始化项目脚本

---
+++ Tools/InitProject.bat
@@ -0,0 +1,7 @@
+REM ## git init
+git pull
+git submodule sync
+git submodule init
+git submodule update
+REM ## git update
+./UpdateAll.bat
--- Tools/UpdateAll.bat
+++ Tools/UpdateAll.bat
@@ -1,8 +1,5 @@
 REM ## git sync
 git pull
-git submodule sync
-git submodule init
-git submodule update -f
 git submodule foreach "git checkout master"
 git submodule foreach "git pull"
 
